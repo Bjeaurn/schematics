93b953dd410ccacad43a23e6ff7f9f71a2cc6122
Bjorn 'Bjeaurn <bjorn@somnu.com>
2018-06-08 10:21:08 +0200

    Cleans up package.json, updates to schematic.

--- src/packageupdate/index.ts
+++ src/packageupdate/index.ts
@@ -2,12 +2,14 @@ import {
   Rule,
   SchematicContext,
   Tree,
-  UpdateRecorder
+  SchematicsException
 } from "@angular-devkit/schematics";
 
 export interface PackageJson {
   scripts: { [key: string]: string };
 }
+
+const packageJsonPath = "/package.json";
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function packageupdate(options: any): Rule {
@@ -15,19 +17,23 @@ export function packageupdate(options: any): Rule {
     // Prevents compiler whine about unused options.
     if (options) {
     }
-    const file = tree.get("/package.json");
-    if (file) {
-      const json: PackageJson = JSON.parse(file.content.toString());
-      const ciTest = json.scripts["test:ci"];
-      if (ciTest) {
-        console.warn("test:ci already exists");
+    _context.logger.debug('Checking and adding missing scripts to package.json');
+    const buffer = tree.read(packageJsonPath);
+    if(buffer === null) {
+      throw new SchematicsException("Could not find package.json");
+    }
+    const packageJson: PackageJson = JSON.parse(buffer.toString());
+
+    if(packageJson) {
+      if (packageJson.scripts['test:ci']) {
+        _context.logger.warn("test:ci already exists");
       } else {
-        json.scripts["test:ci"] = "bliep bloep I have been added automatically";
+        packageJson.scripts["test:ci"] = "bliep bloep I have been added automatically";
       }
     }
-    const test: UpdateRecorder = tree.beginUpdate("/package.json");
-    test.insertLeft(0, "test?");
-    tree.commitUpdate(test);
+    
+    tree.overwrite(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
     return tree;
   };
 }
@@ -39,6 +45,14 @@ So I think I'll have to calculate what index I have to pass it to insert it into
 
 - AST transformations looks like something to look into.
 - Maybe check what @angular/ did here? How they mutated the package.json etc.
+*/
+
+/* 
+// Star Log 20180608 0952
+JSON files are quite easy to parse and overwrite, looked up an example from the Angular/schematics.
+Looking into file manipulation that isn't JSON, seems like more of what I described in the previous log.
+
+Diving into that right now.
 */
 
 // URL references:
